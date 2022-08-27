1b41ec6ed9b979c43f17b50a35145faefabfbf3d
Olga Bobrova <eve982@yandex.ru>
2022-08-27 15:56:04 +0300

    my first blog

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,27 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
-def index(request):    
+
+def index(request):
     template = 'posts/index.html'
-    text = 'Это главная страница проекта Yatube'
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'text': text,
+        'text': title,
+        'posts': posts,
     }
-    return render(request, template, context) 
+    return render(request, template, context)
+
 
 def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     template = 'posts/group_list.html'
-    text = 'Здесь будет информация о группах проекта Yatube'
+    title = 'Записи сообщества' + ' ' + group.title
     context = {
         'slug': slug,
-        'text': text,
+        'text': title,
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -13,9 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# По умолчанию в проект Django подключена система администрирования 
+
 from django.contrib import admin
-# Функция include позволит использовать path() из других файлов.
 from django.urls import include, path
 
 urlpatterns = [
